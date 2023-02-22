4c85822b9c6dd4a18fcfb63065d10f631e159a72
simon <sbredeche2@gmail.fr>
2023-02-22 05:13:07 -0800

    added docker compose for local debug

--- back/index.php
+++ back/index.php
@@ -1,6 +1,11 @@
 <?php 
 error_reporting( E_ALL );
-ini_set( "display_errors", 1 );      
+ini_set( "display_errors", 1 );    
+header('Access-Control-Allow-Origin: *');
+
+header('Access-Control-Allow-Methods: GET, POST');
+
+header("Access-Control-Allow-Headers: X-Requested-With");
 require_once(__DIR__.'/app/Controller.php');
 require_once(__DIR__.'/app/Model.php');
 $params = explode('/',$_SERVER["REQUEST_URI"]);
